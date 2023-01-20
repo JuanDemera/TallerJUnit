034b3f26b033414690609b8386584a04f3fce38e
LuisAntonioQuezadaAristega <107262704+LuisAntonioQuezadaAristega@users.noreply.github.com>
2023-01-19 21:30:13 -0500

    test customer

---
+++ RentalSys/RentalSys/RentalSystem/src/test/java/com/espol/rentalsystemtest/CustomerTest.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.espol.rentalsystemtest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author laque
+ */
+public class CustomerTest {
+
+    @Test
+    public void testAddMovieRental() {
+        MovieRental arg = new MovieRental(new Movie("One piece film red", 15), 12);
+        Customer instance = new Customer("Luis");
+        instance.addMovieRental(arg);
+    }
+
+    @Test
+    public void testAddVideoGameRental() {
+        VideoGameRental arg = new VideoGameRental("To leave", 18, true);
+        Customer instance = new Customer("Luis");
+        instance.addVideoGameRental(arg);
+    }
+
+    @Test
+    public void testStatement() {
+        Customer instance = new Customer("Luis");
+        String expResult = "Rental Record for Luis\n";
+        String result = instance.statement();
+        assertEquals(expResult, result);
+    }
+    
+}
